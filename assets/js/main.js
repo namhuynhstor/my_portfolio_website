d1c6b64477e7bed1230ad8579976e0be0882c26c
Nam Huynh <namhuynh@Nams-MacBook-Pro.local>
2023-06-21 18:41:25 +0700

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,3 +143,23 @@
 			});
 
 })(jQuery);
+
+	      let likes = 0;
+      $(document).ready(function () {
+        // ajax to get current likes
+        // let likes from server are 10
+        // assign the current likes to variable
+        likes = 0;
+        setLikes(likes);
+      });
+      
+      $("body").on("click", ".likeBtn", function () {
+        // ajax to post a current likes
+        // in success add increment to likes
+        likes++;
+        setLikes(likes);
+      });
+
+      function setLikes(count) {
+        $(".totalLikes").text(count);
+      }
